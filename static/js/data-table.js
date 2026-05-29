e954142c0c65b5c5dba4f3c7a0066bfb1b534fb2
mbrangi <matatabrangi@gmail.com>
2026-05-30 02:18:28 +0300

    fix: responsive navbar, sidebar visible on mobile, responsive DataTables

--- static/js/data-table.js
+++ static/js/data-table.js
@@ -2,22 +2,24 @@
   'use strict';
   $(function() {
     $('#order-listing').DataTable({
+      responsive: true,
       "aLengthMenu": [
         [5, 10, 15, -1],
         [5, 10, 15, "All"]
       ],
       "iDisplayLength": 10,
-      "language": {
-        search: ""
-      }
+      "language": { search: "" },
+      columnDefs: [
+        { responsivePriority: 1, targets: 0 },
+        { responsivePriority: 2, targets: 3 },
+        { responsivePriority: 3, targets: -1 }
+      ]
     });
     $('#order-listing').each(function() {
       var datatable = $(this);
-      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
       var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
       search_input.attr('placeholder', 'Search');
       search_input.removeClass('form-control-sm');
-      // LENGTH - Inline-Form control
       var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
       length_sel.removeClass('form-control-sm');
     });
